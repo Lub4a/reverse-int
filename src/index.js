bcac7695a35a16b2dd52f90f8492063043e9d896
Luybov Kurij <lubovhimicka@gmail.com>
2023-08-04 19:37:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+      const nativeNumber = Math.abs(n);
+      const arr = ('' + nativeNumber).split('');
+    const arr2 = arr.reverse();
+    return arr2.join('');
 }
